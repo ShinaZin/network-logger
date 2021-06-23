4e2b388b492053457c3a9867888c151120f81fea
Pavel Zinovev <ShinaZin@mail.ru>
2021-06-23 19:56:13 +0300

    APP: Main dev page improved

--- src/devtools/app/App.tsx
+++ src/devtools/app/App.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
+import { TextInput } from '../react-controls/text-input';
+import { Container } from './styles';
 
 export const App: React.FC = () => (
-  <div>Work in progress</div>
+  <Container>
+    <TextInput defaultValue = 'some text' />
+    <TextInput placeholder = 'some text' />
+  </Container>
 );
