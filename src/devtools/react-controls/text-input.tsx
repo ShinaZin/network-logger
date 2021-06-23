22775f2b3aaa274ae28c1501105ca98e21b990d8
Pavel Zinovev <ShinaZin@mail.ru>
2021-06-23 19:54:24 +0300

    CTL: Implemented TextInput component

---
+++ src/devtools/react-controls/text-input.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import * as bc from '../styles/base';
+import styled from 'styled-components';
+
+export const StyledInput = styled.input`
+  background-color: ${bc.controlBgColor};
+  color: ${bc.textColor};
+  border: 1px solid transparent;
+  padding: 1px;
+  
+  :focus {
+    border-color: ${bc.focusedBorder};
+  }
+
+  :hover {
+    border-color: ${bc.hoveredBorder};
+  }
+`;
+
+const enum KeyCodes {
+  ENTER = 13
+}
+
+interface Props extends React.InputHTMLAttributes<HTMLInputElement>  {
+  onEnter?: (value: string) => void;
+}
+
+export class TextInput extends React.Component<Props> {
+
+  private onEnter = (text: string) => {
+    this.props.onEnter?.(text);
+  };
+
+
+  private onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.keyCode == KeyCodes.ENTER && this.props.onEnter) {
+      e.preventDefault();
+      this.onEnter(e.currentTarget.value);
+    }
+    this.props.onKeyDown?.(e);
+  };
+
+  render() {
+    const { onEnter, value, ...props } = this.props;
+
+    return (
+      <StyledInput
+        value={value}
+        type='text'
+        {...props}
+        onKeyDown={this.onKeyDown}
+      />
+    );
+  }
+}
