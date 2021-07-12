0b4f3ecf216e1fff6d3c56f65c5b87706945225a
Pavel Zinovev <ShinaZin@mail.ru>
2021-07-12 22:32:42 +0300

    APP: Main page stub improved

--- src/devtools/app/App.tsx
+++ src/devtools/app/App.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
-import { TextInput } from 'devtools/react-controls/text-input';
 import { Container } from 'devtools/app/styles';
+import { Border } from 'devtools/react-controls/border';
+import { Box } from 'devtools/react-controls/box';
+import { TextInput } from 'devtools/react-controls/text-input';
 
 export const App: React.FC = () => (
   <Container>
-    <TextInput defaultValue = 'some text' />
-    <TextInput placeholder = 'some text' />
+    <Box style={{ textAlign: 'center', color: 'gray', fontSize: '30px' }} padding>Network logger</Box>
+    <Border top>
+      <Box col spacing style={{ width: '75%', margin: 'auto' }} padding='v'>
+        {[0, 1, 2].map(key => <Item key={key} />)}
+      </Box>
+    </Border>
   </Container>
+);
+
+
+const Item: React.FC = () => (
+  <Box col spacingSm>
+    <TextInput placeholder='/api/request/method' grow />
+    <TextInput placeholder='path.to.item[0].name' />
+  </Box>
 );
