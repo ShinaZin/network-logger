7c9ad642e6040599aec2040d99df01d49954174b
Pavel Zinovev <ShinaZin@mail.ru>
2021-08-15 18:14:50 +0300

    STYLE: Added hovered text color

--- src/devtools/styles/base.ts
+++ src/devtools/styles/base.ts
@@ -1,6 +1,7 @@
 // colors
 export const bgColor = '#3d3d3d';
-export const textColor = '#ffffff';
+export const textColor = '#a5a5a5';
+export const hoveredTextColor = '#c0c0c1';
 export const controlBgColor = '#242424';
 export const focusedBorder = '#063a65';
 export const hoveredBorder = '#7a7a7a';
