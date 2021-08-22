5f473127defece4a1afa643c20250040b03f838d
Pavel Zinovev <ShinaZin@mail.ru>
2021-08-22 14:16:50 +0300

    APP: rules editing via simple store

--- src/devtools/app/rules-list/rules.tsx
+++ src/devtools/app/rules-list/rules.tsx
@@ -1,22 +1,23 @@
 import * as React from 'react';
+import { useForceUpdate } from 'common/hooks';
 import { Border } from 'devtools/react-controls/border';
 import { Box } from 'devtools/react-controls/box';
-import { TextInput } from 'devtools/react-controls/text-input';
 import { IconButton } from 'devtools/react-controls/icon-button';
-import { getDefaultRules } from './default-rules';
+import { TextInput } from 'devtools/react-controls/text-input';
+import { Store } from 'devtools/store/store';
 import { Rule } from './types';
 
 const style: React.CSSProperties = { width: '75%', margin: 'auto' };
 
 export const Rules: React.FC = () => {
-  const [rules, setRules] = React.useState(getDefaultRules());
-  const onChange = () => setRules([...rules]);
+  const forceUpdate = useForceUpdate();
+  React.useEffect(() => Store.setUpdater(forceUpdate), []);
 
   return (
     <Border top>
       <Box col spacing style={style} padding='v'>
-        {rules.map(rule => (
-          <RuleItem key={rule.id} rule={rule} onChange={onChange} />
+        {Store.rules.map((rule, i) => (
+          <RuleItem key={rule.id} index={i} rule={rule} />
         ))}
       </Box>
     </Border>
@@ -24,22 +25,26 @@ export const Rules: React.FC = () => {
 };
 
 interface RuleItemProps {
+  index: number;
   rule: Omit<Rule, 'id'>;
-  onChange: VoidFunction;
 }
 
 const RuleItem: React.FC<RuleItemProps> = (props) => {
-  const { rule, onChange } = props;
+  const { rule, index } = props;
 
   const onPathChange = (path: string) => {
-    rule.path = path;
-    onChange();
+    Store.setRulePath(index, path);
   };
 
   const onUrlChange = (url: string) => {
-    rule.url = url;
-    onChange();
+    Store.setRuleUrl(index, url);
   };
+
+  const onRemove = () => Store.removeRule(index);
+
+  const onAdd = () => Store.insertRule(index);
+
+  const disabled = Store.rules.length == 1;
 
   return (
     <Box row spacing grow>
@@ -48,8 +53,8 @@ const RuleItem: React.FC<RuleItemProps> = (props) => {
         <TextInput placeholder='path.to.item[0].name' value={rule.path} onChange={onPathChange} />
       </Box>
       <Box col spacingSm>
-        <IconButton src='/assets/icons/actions/delete.svg' />
-        <IconButton src='/assets/icons/actions/new.svg' />
+        <IconButton src='/assets/icons/actions/delete.svg' onClick={onRemove} disabled={disabled} />
+        <IconButton src='/assets/icons/actions/new.svg' onClick={onAdd} />
       </Box>
     </Box>
   );
