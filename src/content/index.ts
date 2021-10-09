71499875109be8e488c82387192091df0ef7d19a
Pavel Zinovev <ShinaZin@mail.ru>
2021-10-09 13:03:09 +0300

    APP: basic functionality implemented

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,1 +1,16 @@
-console.log('The content script is running!');
+import { findMatchingRule } from 'core/helpers';
+import { Message } from 'core/types';
+import { logRequestData, logResponseData } from './logger';
+
+chrome.runtime.onMessage.addListener(async (message: Message) => {
+  const rule = await findMatchingRule(message.url);
+  if (!rule) {
+    return;
+  }
+
+  switch (message.type) {
+    case 'request': logRequestData(message, rule); break;
+    case 'response': logResponseData(message, rule); break;
+  }
+});
+
